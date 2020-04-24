e06ff72addbb225fa46c0ab4b1ac0bdcef0bdfde
Héctor Olvera Vital <hector.olvera@ciencias.unam.mx>
2020-04-24 08:59:19 -0500

    challenge solved

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
 function diamond(size) {
-  // your code
-  return;
+  if (size < 1 || size % 2 !== 1) return null;
+  const diamondArray = new Array(size);
+
+  for (let i = 0; i < diamondArray.length; i++) {
+    const dimRow = 2 * i + 1;
+    diamondArray[i] = `${' '.repeat(Math.abs(size - dimRow) / 2)}${'*'.repeat(size - Math.abs(size - dimRow))}`;
+  }
+
+  const diamondString = `${diamondArray.join('\n')}\n`;
+  
+  return diamondString;
 }
 
 module.exports = { diamond };
