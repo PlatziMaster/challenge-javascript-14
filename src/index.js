4109dc1b1b2dfc84300bbe1e558380985cd3b6d7
Iván Chipatecua <ivchip@gmail.com>
2020-04-25 00:13:15 -0500

    Challenge JavaScript 14 completed

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
 function diamond(size) {
-  // your code
-  return;
+  if (size < 0 || size % 2 == 0)
+    return null;
+  let diamond = '';
+
+  for (let index = 0; index < size; index++) {
+    const element = 2 * index + 1;
+    diamond += ' '.repeat(Math.abs(size - element) / 2) + '*'.repeat(size - Math.abs(size - element)) + '\n';
+  }
+
+  return diamond;
 }
 
 module.exports = { diamond };
