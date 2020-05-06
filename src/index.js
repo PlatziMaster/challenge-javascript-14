e7734aa39f159694e60995477b88c29a98015690
Razielini <razielco@gmail.com>
2020-05-05 22:03:15 -0600

    Update index.js
    Solved

--- src/index.js
+++ src/index.js
@@ -1,6 +1,20 @@
 function diamond(size) {
-  // your code
-  return;
+  let res = null
+  let resInvert = ''
+  let cRes = ''
+  if((size%2) > 0 && size > 0) {
+    let interaction = (size - 1) / 2
+    res = ''
+    while(interaction >= 0) {
+      resInvert = cRes + resInvert
+      cRes = ' '.repeat(interaction) + '*'.repeat((size - (interaction * 2))) + '\n'
+      res += cRes
+      interaction--
+    }
+
+    res = res + resInvert
+  }
+  return res
 }
 
 module.exports = { diamond };
