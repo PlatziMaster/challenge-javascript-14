b6a2a1762de4bee6b2b478085c5609692133a34e
Cristian F. Tovar <cristhiam.tovar@mail.escuelaing.edu.co>
2020-05-07 16:36:26 -0500

    Ready to push

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
-function diamond(size) {
-  // your code
-  return;
-}
+(function diamond(size) {
+  if ( size < 1 || !(size % 2)) return null;
+  
+  const diamond = [];
 
-module.exports = { diamond };
+  for (let i = 0; i < size; i++) {
+    const row = (i*2) + 1;
+    const spaces = ' '.repeat(Math.abs(row - size) / 2);
+    const asterisks = '*'.repeat(size - Math.abs(row - size));
+    diamond.push(`${spaces}${asterisks}`);
+  }
+
+  const diamondString = `${diamond.join('\n')}\n`;
+  console.log(diamond)
+  return diamondString;
+})(31)
+
+// module.exports = { diamond };
