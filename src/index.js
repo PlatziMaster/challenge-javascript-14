b4d627240cccd1dded30e913266b037fcae0f1c8
Razielini <razielco@gmail.com>
2020-05-09 14:21:23 -0600

    Update index.js
    Again, lo probe antes y el resultado fue bueno, por si acaso ahora reinicie el IDE

--- src/index.js
+++ src/index.js
@@ -3,14 +3,14 @@ function diamond(size) {
   let resultInvert = ''
   let backupResult = ''
   if((size%2) > 0 && size > 0) {
+    result = ''
     let interaction = (size - 1) / 2
     while(interaction >= 0) {
       resultInvert = backupResult + resultInvert
-      backupResult = `${' '.repeat(interaction)} ${'*'.repeat((size - (interaction * 2)))} \n`
+      backupResult = `${' '.repeat(interaction)}${'*'.repeat((size - (interaction * 2)))}\n`
       result += backupResult
       interaction--
     }
-
     result = result + resultInvert
   }
   return result
