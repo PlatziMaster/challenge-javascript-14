1f048920bfe0ccbdea867a3b4b201f4fe8ee07fe
Jedp <jedp082@gmail.com>
2020-06-01 01:03:00 -0500

    Challenge done :D

--- src/index.js
+++ src/index.js
@@ -1,6 +1,44 @@
-function diamond(size) {
-  // your code
-  return;
+function calculateDistance(startFrom, target){
+  let distance = 0;
+  for(let i = startFrom+1; i <= target; i++){
+    if(i%2 != 0){
+      distance++
+    }
+  }
+  return distance;
 }
 
+function diamond(size) {
+  if(size%2 === 0 || size < 0){
+    return null;
+  }
+  
+  let diamond = '';
+  let nxtLine = ''
+
+  // Top Part
+  for(let i = 1; i < size; i++){
+    if(i%2 != 0){
+      let distance = calculateDistance(i, size);
+      nxtLine += ' '.repeat(distance)
+      nxtLine += '*'.repeat(i);
+      nxtLine += '\n'
+    }
+  }
+  diamond += nxtLine
+
+  // Bottom part
+  nxtLine = '';
+  for(let i = size; i > 0; i--){
+    if(i%2 != 0){
+      let distance = calculateDistance(i, size);
+      nxtLine += ' '.repeat(distance)
+      nxtLine += '*'.repeat(i)
+      nxtLine += '\n'
+    }
+  }
+  diamond += nxtLine
+  return diamond;
+}
+diamond(9)
 module.exports = { diamond };
