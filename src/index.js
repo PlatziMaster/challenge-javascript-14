7d29c3a915581279360c2bb1a0fac8bd6047de2e
AngelicaSier <alandazabal@sier.com.co>
2020-06-07 22:17:38 -0500

    With messages

--- src/index.js
+++ src/index.js
@@ -1,6 +1,68 @@
+function recursiveStr(stars, size){
+  //console.log(stars+"-"+size);
+  //console.log("funcion");
+  let diffBlank = (size - stars)/2;
+
+  let str = "";
+
+  for(let i=0; i < diffBlank; i++){
+    str = str.concat(" ");
+  }
+
+  for(let i=0; i < stars; i++){
+    str = str.concat("*");
+  }
+
+  /* for(let i=0; i < diffBlank; i++){
+    str = str.concat(" ");
+  } */
+
+  str = str.concat("\n");
+
+  //console.log("func"+str);
+  return str;
+
+}
+
 function diamond(size) {
-  // your code
-  return;
+
+  let diamondst = "";
+
+  if ( size % 2 == 0 ){
+    return null;
+  }else if( size < 1 ){
+    return null
+  }else{
+    //console.log("Entrando");
+    for( let i = 1; i <= size; i++){
+      //console.log("For");
+      if( i%2 != 0){
+        //console.log("Condicion");
+        let str = recursiveStr(i, size)
+        //console.log("concat"+str);
+        //diamondst.concat("str","123");
+        diamondst = diamondst + str;
+        //console.log("inside"+diamondst);
+      }
+    }
+
+    for( let i = size-2; i >= 1; i--){
+      //console.log("For");
+      if( i%2 != 0){
+        //console.log("Condicion");
+        let str = recursiveStr(i, size)
+        //console.log("concat"+str);
+        //diamondst.concat("str","123");
+        diamondst = diamondst + str;
+        //console.log("inside"+diamondst);
+      }
+    }
+    
+  }
+
+  console.log(diamondst);
+
+  return diamondst;
 }
 
 module.exports = { diamond };
