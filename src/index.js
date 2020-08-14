29d99d9389037bb03594434f8c626a5284652ca0
juliodev <juliocesardeveloper@gmail.com>
2020-08-14 17:52:38 -0500

    Challenge done!

--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
 function diamond(size) {
-  // your code
-  return;
+  let diamond = '';
+  const space = ' ';
+  const asterisk = '*';
+  if (size < 1 || size % 2 !== 1) {
+    return null;
+  } else {
+    for (let index = 0; index < size; index++) {
+      const odd = 2 * index + 1;
+      diamond += `${space.repeat(Math.abs(size - odd) / 2)}${asterisk.repeat(size - Math.abs(size - odd))}\n`;
+    }
+    return diamond;
+  }
 }
 
 module.exports = { diamond };
