a1e69daef0a70dc137c374e963de514573de4270
rabi <rabi_leon@hotmail.com>
2020-09-05 01:07:29 -0500

    :boom: Reto resuelto

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
 function diamond(size) {
-  // your code
-  return;
+  if (size < 0 || size % 2 === 0) return null;
+  // creamos una variable vacia
+  let diamond = "";
+  for (let i = 0; i < size; i++) {
+    //obtenemos numeros impares
+    const item = 2 * i + 1;
+    //''.repeat(Math.abs((size - item) / 2)) resuelve la cantidad de espcios en blanco agregara al inicio
+    //'*'.repeat(size - Math.abs(size - item)) resuelve cuantas veces se repetira el *
+    diamond += ' '.repeat(Math.abs((size - item) / 2)) + '*'.repeat(size - Math.abs(size - item)) + '\n';
+  }
+  return diamond;
 }
-
 module.exports = { diamond };
