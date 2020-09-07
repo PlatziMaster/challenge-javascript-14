95ae844ce63705cdd8ecb7e4e30bdfb93e785ba9
juan sebastian herrera <juan.herreraca@campusucc.edu.co>
2020-09-07 09:32:54 -0500

    challenge solved

--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
 function diamond(size) {
-  // your code
-  return;
+  if (size < 3 || size %2 === 0) {
+    return null
+  } else {
+    const diamondArray = new Array (size);
+
+    for (let i = 0; i < diamondArray.length; i++ ){
+     const level = 2 * i + 1;
+    diamondArray[i] = `${' '.repeat(Math.abs(size - level) / 2)}${'*'.repeat(size - Math.abs(size - level))}${diamondArray.join('\n')}\n`;
+    }
+    console.log(diamondArray)
+    return diamondArray;
+  }
 }
 
 module.exports = { diamond };
