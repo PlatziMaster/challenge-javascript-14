1a3dac8ff730bda99cac11e784981705bf8c9b5e
andresmesad <andresf.mesad@gmail.com>
2020-11-03 23:46:26 -0500

    challenge solved

--- src/index.js
+++ src/index.js
@@ -1,6 +1,38 @@
 function diamond(size) {
   // your code
-  return;
+  if (size % 2 === 0 || size < 0) {
+    return null
+  } 
+  let printLn = (size, dots) => {
+    let spaces = (size - dots) / 2;
+    let strSpaces = " ".repeat(spaces);
+    let strDots = "*".repeat(dots);
+    return `${strSpaces}${strDots}\n`;
+  }
+
+  finalLn = ''
+
+  // Top 
+  for (let i = 0; i <= Math.floor(size / 2) + 2; i++) {
+    if (i%2 === 0) {
+      finalLn += printLn(size, i+1)
+    } else {
+      continue
+    }
+  }
+
+
+  // Bottom
+  for (let i = size; i >= size / 2; i--) {
+    if (i%2 !== 0) {
+      finalLn += printLn(size, i - 2)
+    } else {
+      continue
+    }
+    
+  }
+
+  return finalLn;
 }
 
 module.exports = { diamond };
